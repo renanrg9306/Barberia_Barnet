09702bc3e5b9f22ab6b304d5fdfdbf1060890aca
Renan Rosales <renanrg9306@gmail.com>
2023-11-26 14:53:14 -0600

    Diseno Barberia Barner

---
+++ header.php
@@ -0,0 +1,34 @@
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand">
+            <span>
+              Barberia Barner
+            </span>
+          </a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=""> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav  mx-auto ">
+              <li class="nav-item active">
+                <a class="nav-link">Inicio <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link">Galeria</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link">Acerca de nostros</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link">Reserva una cita</a>
+              </li>
+            </ul>
+            </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
